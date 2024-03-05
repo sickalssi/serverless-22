2b2d761b3a0a1e7e7a157ed33191754eb025a163
sickalssi <furysworder@hotmail.com>
2024-03-05 19:47:27 +0800

    added cloudwatch logging into indexjs

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 ## 28-Feb-2024
 Added job to retrieve secrets stored in AWS Secrets Manager into workflow yml. 
  - Added aws region into repository secrets to get rid of exit 255 error.
+
+# 05-Mar-2024
+Added cloudwatch logging capabilities 
--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-module.exports.handler = async (event) => {
-    return {
-      statusCode: 200,
-      body: JSON.stringify(
-        {
-          message: "Meow Meow",
-        },
-        null,
-        2
-      ),
-    };
+module.exports.delfrinando = async (event) => {
+  console.log(new Date() + " Second Last Session Of Cohort 4")
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: "Meow Meow",
+      },
+      null,
+      2
+    ),
   };
+};
